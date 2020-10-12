13047fabd1837d7c6abf132f6b76ea4b3bce9bc8
“Becca” <r.e.burten@gmail.com>
2020-10-12 12:16:12 -0700

    Set up basic layout, include stones and bonzais imgs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import "./styles/index.scss";
+function draw(){
+    let sandbox = document.getElementById("sandbox");
+    let c = sandbox.getContext("2d");
+
+    c.beginPath;
+    c.moveTo(200, 200);
+    c.lineTo(200, 300);
+    c.stroke();
+
+
+    let stone2 = document.getElementById("stone2");
+    c.drawImage(stone2, 250, 250, 50, 50);
+} 
+
+draw();
---
+++ src/styles/styles.md
@@ -0,0 +1,6 @@
+// color palette:
+// green: #495925
+// light green: #93A64E
+// gray/sand: #A69D8D 
+// brown/stone: #261308
+// font-family: 'Seaweed Script', cursive;
