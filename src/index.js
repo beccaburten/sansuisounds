4ff571920762ed42ca5d206bca82668c22e4b5c9
“Becca” <r.e.burten@gmail.com>
2020-10-12 16:32:40 -0700

    Proper image render in canvas

--- src/index.js
+++ src/index.js
@@ -3,28 +3,28 @@ import './styles/navbar.scss';
 import './styles/canvas.scss';
 import './styles/garden-items.scss';
 
-
 function draw(){
     let sandbox = document.getElementById("sandbox");
+    sandbox.width = window.innerWidth;
+    sandbox.height = 0.75 * window.innerHeight;
     let c = sandbox.getContext("2d");
     c.imageSmoothingEnabled = false;
 
-    // c.arc(600, 600, 50, 0, Math.PI * 2, false);
-    // c.beginPath();
-    // c.stroke();
+    // var stone1 = document.getElementById("stone1");
+    // c.drawImage(stone1, 25, 25, 70);
 
-
-    var stone1 = document.getElementById("stone1");
-    stone1.onload = function() {
-        c.drawImage(stone1, 25, 25, 25, 25);
-    };
 
     let tree = new Image();
-    tree.src = "./src/images/garden-items/bonzai-2.png";
     tree.onload = function(){
-        c.drawImage(tree, 100, 100, 20, 20);
+        // c.drawImage(tree, 100, 100, 30, 40);
+        c.drawImage(tree, 20, 20);
     }
+    tree.src = "./src/images/garden-items/bonzai-2.png";
+
     debugger;
 } 
 
-draw();
+
+document.addEventListener("DOMContentLoaded", () => {
+    draw();  
+})
