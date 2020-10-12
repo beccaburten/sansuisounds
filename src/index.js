221967ded993c3d2b4621e7192702765a0d78c60
“Becca” <r.e.burten@gmail.com>
2020-10-12 14:10:45 -0700

    Image renders to canvas, very pixelated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
+import './styles/index.scss';
+import './styles/navbar.scss';
+import './styles/canvas.scss';
+import './styles/garden-items.scss';
+
+
 function draw(){
     let sandbox = document.getElementById("sandbox");
     let c = sandbox.getContext("2d");
+    c.imageSmoothingEnabled = false;
 
-    c.beginPath;
-    c.moveTo(200, 200);
-    c.lineTo(200, 300);
-    c.stroke();
+    // c.arc(600, 600, 50, 0, Math.PI * 2, false);
+    // c.beginPath();
+    // c.stroke();
 
 
-    let stone2 = document.getElementById("stone2");
-    c.drawImage(stone2, 250, 250, 50, 50);
+    var stone1 = document.getElementById("stone1");
+    stone1.onload = function() {
+        c.drawImage(stone1, 25, 25, 25, 25);
+    };
+
+    let tree = new Image();
+    tree.src = "./src/images/garden-items/bonzai-2.png";
+    tree.onload = function(){
+        c.drawImage(tree, 100, 100, 20, 20);
+    }
+    debugger;
 } 
 
 draw();
