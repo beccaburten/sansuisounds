924a469f684d96f701277ab205c8b3541b32722c
“Becca” <r.e.burten@gmail.com>
2020-10-12 21:05:47 -0700

    Play buttons for Rake and Clear both work

--- src/index.js
+++ src/index.js
@@ -16,83 +16,93 @@ document.addEventListener("DOMContentLoaded", () => {
     let tree = new Image();
     tree.src = "./src/images/garden-items/bonzai-2.png";  
     
-    draw();
+    document.querySelector("#clear-canvas").addEventListener("click", clearCanvas);
 
-    function draw(){
-        c.clearRect(0,0, w, h)
-        tree.onload = function(){
-            c.drawImage(tree, 200, 20);
-        }
-    } 
-
-    function distanceMoved(x, y, item) {
-        function distanceXY (x0, y0, x1, y1) {
-            let dx = x1 - x0, dy = y1 - y0;
-            return Math.sqrt(dx * dx + dy * dy);
-        }
-		return distanceXY(x, y, item.x, item.y) < (item.width / 2);
+    function clearCanvas() {
+        c.clearRect(0, 0, w, h);
     }
+
+    document.querySelector("#rake-play").addEventListener("click", animateRake);
     
-    document.addEventListener("mousedown", (e)=>{
-            if(distanceMoved(e.clientX, e.clientY, tree)) {
-                document.addEventListener("mousemove", onMouseMove);
-                document.addEventListener("mouseup", onMouseUp);
-            }
-        }
-    )
+    function Rake(y, dX, style) {
+        let cX = 0;
+        this.y = y;
+        this.dX = dX;
+        this.style = style;
 
-    function onMouseMove(e) {
-        tree.x = e.clientX;
-        tree.y = e.clientY;
-        draw();
+        this.draw = function() {
+            for (let i = 0; i < 5; i++) {
+                let topY = this.y + (i * 10);
+                c.beginPath();
+                c.moveTo(0, topY);
+                c.lineTo(cX, topY);
+                c.strokeStyle = this.style;
+                c.stroke();
+            }
+
+        }
+        this.move = function() {
+            if (cX === w ) {
+                this.y += 50;
+                cX = 0;
+            }
+            cX += this.dX;
+
+            this.draw();
+        }
     }
 
-    function onMouseUp() {
-        document.removeEventListener("mousemove", onMouseMove);
-        document.removeEventListener("mouseup", onMouseUp);
+    let rakeB = new Rake(10, 2,'#261308')
+    let rakeW = new Rake(9, 2, 'white')
+
+    function animateRake() {
+        requestAnimationFrame(animateRake)
+
+        rakeW.move();
+        rakeB.move();
+        
+        if (rakeB.y >= w ) {
+            rakeB.dX = 0;
+            rakeW.dX = 0;
+        }
     }
 
 })
 
-function Rake(y, dX, style) {
-    let cX = 0;
-    this.y = y;
-    this.dX = dX;
-    this.style = style;
 
-    this.draw = function() {
-        for (let i = 0; i < 5; i++) {
-            let topY = this.y + (i * 10);
-            c.beginPath();
-            c.moveTo(0, topY);
-            c.lineTo(cX, topY);
-            c.strokeStyle = this.style;
-            c.stroke();
-        }
 
-    }
-    this.move = function() {
-        if (cX === w ) {
-            this.y += 50;
-            cX = 0;
-        }
-        cX += this.dX;
 
-        this.draw();
-    }
-}
+// putting this inside DOMContentLoaded will render a tree
+    // draw();
 
-let rakeB = new Rake(10, 2,'#261308')
-let rakeW = new Rake(9, 2, 'white')
+    // function draw(){
+    //     c.clearRect(0,0, w, h)
+    //     tree.onload = function(){
+    //         c.drawImage(tree, 200, 20);
+    //     }
+    // } 
 
-function animateRake() {
-    requestAnimationFrame(animate)
-
-    rakeW.move();
-    rakeB.move();
-    
-    if (rakeB.y >= w ) {
-        rakeB.dX = 0;
-        rakeW.dX = 0;
-    }
-}
+// mouse listeners/dragging DOES NOT work
+    // function distanceMoved(x, y, item) {
+    //     function distanceXY (x0, y0, x1, y1) {
+    //         let dx = x1 - x0, dy = y1 - y0;
+    //         return Math.sqrt(dx * dx + dy * dy);
+    //     }
+	// 	return distanceXY(x, y, item.x, item.y) < (item.width / 2);
+    // }
+    // document.addEventListener("mousedown", (e)=>{
+    //         if(distanceMoved(e.clientX, e.clientY, tree)) {
+    //             document.addEventListener("mousemove", onMouseMove);
+    //             document.addEventListener("mouseup", onMouseUp);
+    //         }
+    //     }
+    // )
+    // function onMouseMove(e) {
+    //     tree.x = e.clientX;
+    //     tree.y = e.clientY;
+    //     draw();
+    // }
+    // function onMouseUp() {
+    //     document.removeEventListener("mousemove", onMouseMove);
+    //     document.removeEventListener("mouseup", onMouseUp);
+    // }
--- src/styles/styles.md
+++ src/styles/styles.md
@@ -3,4 +3,5 @@
 // light green: #93A64E
 // gray/sand: #A69D8D 
 // brown/stone: #261308
+// accent/rust: #A6483F
 // font-family: 'Seaweed Script', cursive;
